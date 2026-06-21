3270c239cb2f004987e65cb300ab8bf37c15f0b2
SimoneZoroddu <lenviur@gmail.com>
2026-06-21 18:19:55 +0200

    Step 1 Implement Pizza management features with controller, repository, and views

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/model/Pizza.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -1,5 +1,80 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.model;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "pizza")
 public class Pizza {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    private String name;
+
+    @Lob
+    private String description;
+
+    @NotBlank
+    private String url;
+
+    @NotNull
+    private BigDecimal price;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String toString() {
+        return String.format(
+                "La pizza col nome %s \n Id numero: %d \n Prezzo %s \n Url: %s \n Descrizione della pizza: %s",
+                this.name, this.id, this.price, this.url, this.description);
+    }
+
 }
